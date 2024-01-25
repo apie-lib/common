ad477b0f6d30958334e009a248a31d162e383963
pjordaan <pjordaan@users.noreply.github.com>
2024-01-25 15:10:27 +0000

    Update file(s) "packages/common/." from "apie-lib/apie-lib-monorepo"

--- src/IntegrationTestLogger.php
+++ src/IntegrationTestLogger.php
@@ -1,6 +1,7 @@
 <?php
 namespace Apie\Common;
 
+use PHPUnit\Framework\TestCase;
 use Throwable;
 
 final class IntegrationTestLogger
@@ -11,6 +12,19 @@ final class IntegrationTestLogger
      */
     private function __construct()
     {
+    }
+
+    public static function failTestShowError(int $statusCode = 500): never
+    {
+        TestCase::fail(
+            sprintf(
+                'Failed request, got status %s, logged exception: %s' . PHP_EOL . '%s',
+                $statusCode,
+                self::$loggedException?->getMessage(),
+                self::$loggedException?->getTraceAsString()
+            )
+        );
+        
     }
 
     public static function resetLoggedException(): void
