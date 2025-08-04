5e4232a9754ec3453af99063eee7775e2c9ba0e8
pjordaan <pjordaan@users.noreply.github.com>
2025-08-04 07:45:05 +0000

    Update file(s) "packages/common/." from "apie-lib/apie-lib-monorepo"

--- src/Actions/RunAction.php
+++ src/Actions/RunAction.php
@@ -1,6 +1,7 @@
 <?php
 namespace Apie\Common\Actions;
 
+use Apie\Common\IntegrationTestLogger;
 use Apie\Common\ValueObjects\DecryptedAuthenticatedUser;
 use Apie\Core\Actions\ActionResponse;
 use Apie\Core\Actions\ActionResponseStatus;
@@ -13,7 +14,7 @@ use Apie\Core\Context\ApieContext;
 use Apie\Core\ContextConstants;
 use Apie\Core\Entities\EntityInterface;
 use Apie\Core\Lists\StringList;
-use Apie\Serializer\Exceptions\ValidationException;
+use Exception;
 use LogicException;
 use ReflectionClass;
 use ReflectionMethod;
@@ -53,15 +54,16 @@ final class RunAction implements MethodActionInterface
         if ($alreadyCalculated instanceof ActionResponse) {
             return $alreadyCalculated;
         }
-        $context->withContext(ContextConstants::APIE_ACTION, __CLASS__)->checkAuthorization();
-        $method = new ReflectionMethod(
-            $context->getContext(ContextConstants::SERVICE_CLASS),
-            $context->getContext(ContextConstants::METHOD_NAME)
-        );
-        $object = $method->isStatic()
-            ? null
-            : $context->getContext($context->getContext(ContextConstants::SERVICE_CLASS));
+        
         try {
+            $context->withContext(ContextConstants::APIE_ACTION, __CLASS__)->checkAuthorization();
+            $method = new ReflectionMethod(
+                $context->getContext(ContextConstants::SERVICE_CLASS),
+                $context->getContext(ContextConstants::METHOD_NAME)
+            );
+            $object = $method->isStatic()
+                ? null
+                : $context->getContext($context->getContext(ContextConstants::SERVICE_CLASS));
             $returnValue = $this->apieFacade->denormalizeOnMethodCall($rawContents, $object, $method, $context);
             if ($context->getContext(ContextConstants::METHOD_NAME) === 'verifyAuthentication') {
                 if ($returnValue instanceof EntityInterface) {
@@ -75,7 +77,8 @@ final class RunAction implements MethodActionInterface
                 }
                 $context = $context->withContext(DecryptedAuthenticatedUser::class, $userValue);
             }
-        } catch (ValidationException $error) {
+        } catch (Exception $error) {
+            IntegrationTestLogger::logException($error);
             return ActionResponse::createClientError($this->apieFacade, $context, $error);
         }
         return ActionResponse::createRunSuccess($this->apieFacade, $context, $returnValue, $object);
